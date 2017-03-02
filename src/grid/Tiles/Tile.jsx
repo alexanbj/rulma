b2d87200af977aca5413819dd30dd0c526e7c3f8
Alexander Bjerkan <alb@knowit.no>
2017-03-02 16:44:38 +0100

    Tiles!
    Now able to recreate the first Tile example from Bulma as a story

--- src/grid/Tiles/Tile.jsx
+++ src/grid/Tiles/Tile.jsx
@@ -2,22 +2,37 @@ import React, { PropTypes } from 'react';
 
 import classNames, { modifierPropTypes } from '../../modifiers';
 
-const Tile = ({ ancestor, child, parent, vertical, ...props }) => {
+const Tile = ({ ancestor, child, children, cols, parent, vertical, ...props }) => {
   const [classes, restProps] = classNames(props, 'tile', {
     'is-ancestor': ancestor,
     'is-child': child,
     'is-parent': parent,
     'is-vertical': vertical,
-  });
+    [`is-${cols}`]: cols,
+  }, child ? React.Children.only(children).props.className : null);
+
+  /**
+  * We want child tiles to evenly fill their parent tiles, so we apply classes
+  * directly to the children element here to get the wanted effect.
+  */
+  if (child) {
+    return React.cloneElement(
+      React.Children.only(children),
+      { className: classes, ...restProps },
+    );
+  }
 
   return (
-    <div className={classes} {...restProps} />
+    <div className={classes} {...restProps}>
+      {children}
+    </div>
   );
 };
 
 Tile.propTypes = {
   ancestor: PropTypes.bool,
   child: PropTypes.bool,
+  cols: PropTypes.oneOf(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']),
   ...modifierPropTypes,
   parent: PropTypes.bool,
   vertical: PropTypes.bool,
@@ -26,6 +41,7 @@ Tile.propTypes = {
 Tile.defaultProps = {
   ancestor: false,
   child: false,
+  cols: null,
   parent: false,
   vertical: false,
 };
--- stories/tile.jsx
+++ stories/tile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
-import { Container, Tile, Notification } from '../src/';
+import { Container, Content, Notification, Tile, Title, Subtitle } from '../src/';
 
 storiesOf('Tiles', module)
   .addDecorator(story => (
@@ -8,22 +8,67 @@ storiesOf('Tiles', module)
       {story()}
     </Container>
   ))
-  .add(null, () =>
+  .add('Tiles', () =>
     <Tile ancestor>
-      <Tile vertical parent>
-        <Tile child>
-          <Notification color="primary">
-            Vertical...
-          </Notification>
-          <Notification color="warning">
-            ...tiles
-          </Notification>
+      <Tile vertical parent cols="8">
+        <Tile>
+          <Tile parent vertical>
+            <Tile child>
+              <Notification color="primary">
+                <Title>Vertical...</Title>
+                <Subtitle>Top tile</Subtitle>
+              </Notification>
+            </Tile>
+            <Tile child>
+              <Notification color="warning">
+                <Title>...tiles</Title>
+                <Subtitle>Bottom tile</Subtitle>
+              </Notification>
+            </Tile>
+          </Tile>
+          <Tile parent>
+            <Tile child>
+              <Notification color="info">
+                <Title>Middle tile</Title>
+              </Notification>
+            </Tile>
+          </Tile>
+        </Tile>
+        <Tile parent>
+          <Tile child>
+            <Notification color="danger">
+              <Title>Wide tile</Title>
+              <Subtitle>Aligned with the right tile</Subtitle>
+            </Notification>
+          </Tile>
         </Tile>
       </Tile>
       <Tile parent>
         <Tile child>
-          <Notification color="danger">
-            Wide tile
+          <Notification color="success">
+            <Title>Tall tile</Title>
+            <Subtitle>With even more content</Subtitle>
+            <Content>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Etiam semper diam at erat pulvinar,at pulvinar felis blandit.
+              Vestibulum volutpat tellus diam, consequat gravida libero rhoncus ut.
+              Morbi maximus, leo sit amet vehicula eleifend,
+              nunc dui porta orci, quis semper odio felis ut quam.
+
+              Suspendisse varius ligula in molestie lacinia.
+              Maecenas varius eget ligula a sagittis.
+              Pellentesque interdum, nisl nec interdum maximus, augue diam porttitor lorem,
+              et sollicitudin felis neque sit amet erat.
+              Maecenas imperdiet felis nisi, fringilla luctus felis hendrerit sit amet.
+              Aenean vitae gravida diam, finibus dignissim turpis.
+              Sed eget varius ligula, at volutpat tortor.
+
+              Integer sollicitudin, tortor a mattis commodo, velit urna rhoncus erat,
+              vitae congue lectus dolor consequat libero.
+              Donec leo ligula, maximus et pellentesque sed, gravida a metus.
+              Cras ullamcorper a nunc ac porta.
+              Aliquam ut aliquet lacus, quis faucibus libero. Quisque non semper leo.
+            </Content>
           </Notification>
         </Tile>
       </Tile>

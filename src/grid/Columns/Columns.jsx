8c3c6cfaaae539dd77b43e6fdb55130d3bfdb9ed
Alexander Bjerkan <alb@knowit.no>
2017-03-06 14:16:07 +0100

    Add vCentered prop to Columns
    Use for centered vertical alignment of columns

--- src/grid/Columns/Columns.jsx
+++ src/grid/Columns/Columns.jsx
@@ -2,10 +2,11 @@ import React, { PropTypes } from 'react';
 
 import classNames from '../../modifiers';
 
-const Columns = ({ gapless, multiline, responsive, ...props }) => {
+const Columns = ({ gapless, multiline, responsive, vCentered, ...props }) => {
   const [classes, restProps] = classNames(props, 'columns', {
     'is-gapless': gapless,
     'is-multiline': multiline,
+    'is-vcentered': vCentered,
     [`is-${responsive}`]: responsive,
   });
 
@@ -18,12 +19,14 @@ Columns.propTypes = {
   gapless: PropTypes.bool,
   multiline: PropTypes.bool,
   responsive: PropTypes.oneOf(['mobile', 'desktop']), // By default, columns are activated from tablets and upwards. If you want columns for mobile too, set responsive accordingly.
+  vCentered: PropTypes.bool,
 };
 
 Columns.defaultProps = {
   gapless: false,
   multiline: false,
   responsive: null,
+  vCentered: false,
 };
 
 export default Columns;

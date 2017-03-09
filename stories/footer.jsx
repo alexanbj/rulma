d0d32ee1b391aca0c6e43df5c46167774e00b18a
Alexander Bjerkan <alb@knowit.no>
2017-03-09 12:57:05 +0100

    Proper Footer example in Storybook
    The footer example in the storybook now has actual content instead of a
    TODO

--- stories/footer.jsx
+++ stories/footer.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
-import { Container, Footer } from '../src/';
+import { Container, Content, Footer, Icon } from '../src/';
 
 storiesOf('Footer', module)
   .add('Footer', () =>
     <Footer>
       <Container>
-        TODO: Something cool here
+        <Content textCentered>
+          <p>
+            <strong>rulma</strong>, a React component library for <a href="https://github.com/jgthms/bulma">Bulma</a>.
+            The source code is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+          </p>
+          <p>
+            <a href="https://github.com/alexanbj/rulma">
+              <Icon icon="github" />
+            </a>
+          </p>
+        </Content>
       </Container>
     </Footer>,
   );

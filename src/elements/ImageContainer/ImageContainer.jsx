379e9e0f6a58603f0e6d7a8019213b8ed2d042df
Alexander Bjerkan <alb@knowit.no>
2017-03-28 07:19:54 +0200

    Custom element support for <ImageContainer />

--- src/elements/ImageContainer/ImageContainer.jsx
+++ src/elements/ImageContainer/ImageContainer.jsx
@@ -1,23 +1,25 @@
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 
-const ImageContainer = ({ ratio, dimension, ...props }) => {
+const ImageContainer = ({ ratio, dimension, tag: Tag, ...props }) => {
   const classes = classNames('image', {
     [`is-${dimension}`]: dimension,
     [`is-${ratio}`]: ratio,
   });
 
-  return <figure className={classes} {...props} />;
+  return <Tag className={classes} {...props} />;
 };
 
 ImageContainer.propTypes = {
   dimension: PropTypes.oneOf(['16x16', '24x24', '32x32', '48x48', '64x64', '96x96', '128x128']),
   ratio: PropTypes.oneOf(['square', '1by1', '4by3', '3by2', '16by9', '2by1']),
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
 };
 
 ImageContainer.defaultProps = {
   dimension: null,
   ratio: null,
+  tag: 'div',
 };
 
 export default ImageContainer;

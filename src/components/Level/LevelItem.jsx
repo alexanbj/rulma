b8a84f041a2e4637237b94aebc9fec6cf59c09cd
Alexander Bjerkan <alb@knowit.no>
2017-04-06 21:15:47 +0200

    Add narrow prop to LevelItem

--- src/components/Level/LevelItem.jsx
+++ src/components/Level/LevelItem.jsx
@@ -2,17 +2,19 @@ import React, { PropTypes } from 'react';
 
 import modifiers from '../../modifiers';
 
-const LevelItem = ({ tag: Tag, ...props }) => {
-  const [classes, restProps] = modifiers(props, 'level-item');
+const LevelItem = ({ narrow, tag: Tag, ...props }) => {
+  const [classes, restProps] = modifiers(props, 'level-item', { 'is-narrow': narrow });
 
   return <Tag className={classes} {...restProps} />;
 };
 
 LevelItem.propTypes = {
+  narrow: PropTypes.bool,
   tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
 };
 
 LevelItem.defaultProps = {
+  narrow: false,
   tag: 'div',
 };
 

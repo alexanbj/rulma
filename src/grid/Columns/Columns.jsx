f2193b4b302e77197fe1eb91868b2e5727fa44e5
Alexander Bjerkan <alb@knowit.no>
2017-05-05 08:08:42 +0200

    Custom element support for <Columns />

--- src/grid/Columns/Columns.jsx
+++ src/grid/Columns/Columns.jsx
@@ -3,28 +3,33 @@ import PropTypes from 'prop-types';
 
 import classNames from '../../modifiers';
 
-const Columns = ({ gapless, multiline, responsive, vCentered, ...props }) => {
+const Columns = ({ centered, gapless, multiline, responsive, tag: Tag, vCentered, ...props }) => {
   const [classes, restProps] = classNames(props, 'columns', {
+    'is-centered': centered,
     'is-gapless': gapless,
     'is-multiline': multiline,
     'is-vcentered': vCentered,
     [`is-${responsive}`]: responsive,
   });
 
-  return <div className={classes} {...restProps} />;
+  return <Tag className={classes} {...restProps} />;
 };
 
 Columns.propTypes = {
+  centered: PropTypes.bool,
   gapless: PropTypes.bool,
   multiline: PropTypes.bool,
   responsive: PropTypes.oneOf(['mobile', 'desktop']), // By default, columns are activated from tablets and upwards. If you want columns for mobile too, set responsive accordingly.
-  vCentered: PropTypes.bool,
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  vCentered: PropTypes.bool, // Vertically center the columns
 };
 
 Columns.defaultProps = {
+  centered: false,
   gapless: false,
   multiline: false,
   responsive: null,
+  tag: 'div',
   vCentered: false,
 };
 

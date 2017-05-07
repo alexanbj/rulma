39918c842886ca803c947e2e44bd8ccbb354f70d
Alexander Bjerkan <alb@knowit.no>
2017-05-07 15:42:18 +0200

    Custom element support for <Content /> and <Heading />

--- src/elements/Content/Content.jsx
+++ src/elements/Content/Content.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import classNames, { modifierPropTypes, sizePropType } from '../../modifiers';
 
@@ -8,18 +9,20 @@ import classNames, { modifierPropTypes, sizePropType } from '../../modifiers';
  * When you can't use the components you want, or when you just want to directly use HTML tags,
  * use content as container. It can handle almost any HTML.
  */
-const Content = (props) => {
+const Content = ({ tag: Tag, ...props }) => {
   const [classes, restProps] = classNames(props, 'content');
-  return <div className={classes} {...restProps} />;
+  return <Tag className={classes} {...restProps} />;
 };
 
 Content.propTypes = {
   ...modifierPropTypes,
   size: sizePropType, // Change the font size
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
 };
 
 Content.defaultProps = {
   size: null,
+  tag: 'div',
 };
 
 export default Content;
--- src/elements/Heading/Heading.jsx
+++ src/elements/Heading/Heading.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import classNames from '../../modifiers';
 
-const Heading = (props) => {
+const Heading = ({ tag: Tag, ...props }) => {
   const [classes, restProps] = classNames(props, 'heading');
-  return <p className={classes} {...restProps} />;
+  return <Tag className={classes} {...restProps} />;
+};
+
+Heading.propTypes = {
+  className: PropTypes.string,
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+};
+
+Heading.defaultProps = {
+  className: null,
+  tag: 'p',
 };
 
 export default Heading;

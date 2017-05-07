de10b62c82543c9657d8482c2ed1b03687a197e5
Alexander Bjerkan <alb@knowit.no>
2017-05-07 11:04:26 +0200

    Add prop to CardHeader for easy rendering of title

--- src/components/Card/CardHeader.jsx
+++ src/components/Card/CardHeader.jsx
@@ -2,19 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
+import CardTitle from './CardTitle';
 import CardHeaderIcon from './CardHeaderIcon';
 
-const CardHeader = ({ className, ...props }) => {
+const CardHeader = ({ children, className, title, ...props }) => {
   const classes = classNames('card-header', className);
-  return <header className={classes} {...props} />;
+  return (
+    <header className={classes} {...props}>
+      {title
+        ? [<CardTitle key="title">{title}</CardTitle>, children]
+        : children}
+    </header>
+  );
 };
 
 CardHeader.propTypes = {
+  children: PropTypes.node,
   className: PropTypes.string,
+  title: PropTypes.node, // The title/heading of the Card
 };
 
 CardHeader.defaultProps = {
+  children: null,
   className: null,
+  title: null,
 };
 
 CardHeader.Icon = CardHeaderIcon;

3eace62dff8018ce87560c65cf138ae1840b5487
Alexander Bjerkan <alb@knowit.no>
2017-06-14 08:13:24 +0200

    Add flexible prop to LevelItem

--- src/components/Level/LevelItem.jsx
+++ src/components/Level/LevelItem.jsx
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types';
 
 import modifiers from '../../modifiers';
 
-const LevelItem = ({ as: ElementType, narrow, ...props }) => {
+const LevelItem = ({ as: ElementType, flexible, narrow, ...props }) => {
   const [classes, restProps] = modifiers(props, 'level-item', {
     'is-narrow': narrow,
+    'is-flexible': flexible,
   });
 
   return <ElementType className={classes} {...restProps} />;
@@ -13,11 +14,13 @@ const LevelItem = ({ as: ElementType, narrow, ...props }) => {
 
 LevelItem.propTypes = {
   as: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  flexible: PropTypes.bool,
   narrow: PropTypes.bool,
 };
 
 LevelItem.defaultProps = {
   as: 'div',
+  flexible: false, // Grow the Item (applies only within Level Left/Right)
   narrow: false,
 };
 
